42233c8240a3669661ef5d8639ed24069794f146
Joe <rexno7@gmail.com>
2022-01-28 22:14:40 -0600

    creates ev counters for a given pokemon name

--- ev-helper.js
+++ ev-helper.js
@@ -1,27 +1,54 @@
-const attNames = ['HP', 'Atk', 'Def', 'Sp Atk', 'Sp Def', 'Spd'];
-let name;
-let plus;
-let minus;
-let value;
+const newEvCounter = () => {
+  const pokemonNameTextBox = document.getElementById('add-pokemon-text');
+  const pokemonName = pokemonNameTextBox.value;
+  pokemonNameTextBox.value = '';
 
-const init = (pokemonName, statName) => {
-  this.pokemonName = pokemonName;
-  this.statName = statName;
-}
+  const evCounter = document.createElement('div');
+  evCounter.className = 'ev-counter';
 
-const pokemonCounter = (name) => {
-  console.log(name);
-  let attributes = [ 0, 0, 0, 0, 0, 0 ];
-}
+  const pokemonNameElem = document.createElement('div');
+  pokemonNameElem.className = 'pokemon-name';
+  pokemonNameElem.innerText = pokemonName;
 
-const increment = (name) => {
-  value.innerHTML = value + 1;
-}
+  evCounter.appendChild(pokemonNameElem);
+  evCounter.appendChild(createStat('HP'));
+  evCounter.appendChild(createStat('ATK'));
+  evCounter.appendChild(createStat('DEF'));
+  evCounter.appendChild(createStat('Sp ATK'));
+  evCounter.appendChild(createStat('Sp DEF'));
+  evCounter.appendChild(createStat('SPD'));
 
-const decrement = (name) => {
-  const val = document.getElementById('bulbasaur-hp-value');
-  console.log(val.innerText);
-  console.log(val.innerHTML);
-  // if ()
-  // value.innerHTML = val.innerText - 1;
-}
+  document.getElementById("pokemon-evs-table").appendChild(evCounter);
+};
+
+const createStat = (name) => {
+  let value = 0;
+
+  const statCounter = document.createElement('div');
+  statCounter.className = 'stat-counter';
+
+  const nameElem = document.createElement('div');
+  nameElem.className = 'stat-name';
+  nameElem.innerText = name;
+
+  const valueElem = document.createElement('div');
+  valueElem.className = 'stat-value';
+  valueElem.innerText = value;
+
+  const incrementBtn = document.createElement('button');
+  incrementBtn.className = 'stat-plus';
+  incrementBtn.innerText = '+';
+  incrementBtn.onclick = () => { valueElem.innerText = ++value };
+
+  const decrementBtn = document.createElement('button');
+  decrementBtn.className = 'stat-minus';
+  decrementBtn.innerText = '-';
+  decrementBtn.onclick = () => { valueElem.innerText = value > 0 ? --value : 0 };
+
+  statCounter.appendChild(nameElem);
+  statCounter.appendChild(incrementBtn);
+  statCounter.appendChild(decrementBtn);
+  statCounter.appendChild(valueElem);
+
+  return statCounter;
+};
